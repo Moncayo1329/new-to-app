7dd53a923b4708db56d0c8773439c033fecc204c
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-12-12 17:58:51 +0000

    onclick

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,11 +1,23 @@
+import { GrAddCircle } from "react-icons/gr";
 
 function CreateTodoButton() {
     return(
-  <button>Agrega actividades</button>
+  <button className="CreateTodoButton" 
+  onClick={
+  (event) => {
+  console.log('actividad add')
+  console.log(event)
+  console.log(event.target)
+
+  }
+}
+  ><GrAddCircle /></button>
     );
     }
 
 
 export {CreateTodoButton}
+
+
 
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,17 @@
 function TodoSearch() {
     return(
-    <input placeholder="Ir al gym"/>
+    <input
+     placeholder="Ir al gym"
+          className='TodoSearch'
+         onChange={(event) => {
+            console.log('Escribiste en search');
+            console.log(event);
+            console.log(event.target);
+            console.log(event.target.value);
+
+          } 
+        }
+    />
     );
     }
 
